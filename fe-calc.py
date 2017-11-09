d541a571b8184cae36ae4607e90ae61cc471f7ec
Max Tyler <mtyler88@gmail.com>
2017-11-09 11:42:20 +0000

    Change fe-calc.py

--- fe-calc.py
+++ fe-calc.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-lats = np.arange(10.2, 10.5, 0.01)
-l_str = lambda x: "si.{lat:.2f}".format(lat=x)
+lats = np.arange(10.2, 10.37, 0.01)
+fe_lat_temp = []
 for i in lats: 
-    print(l_str(i))
+    fe_str = "si.{lat:.2f}/si.{lat:.2f}.fe".format(lat=i)
+    fe_lat_temp.append(np.loadtxt(fe_str).transpose().tolist())
+
+LAT, TEMP = np.meshgrid(lats, fe_lat_temp[0][0])
+FREE_ENERGIES = np.array([fe_lat_temp[i][1] for i in range(len(lats))]).transpose()
+print(np.argmin(FREE_ENERGIES, 1))
+
+plt.contourf(LAT, TEMP, FREE_ENERGIES, 100)
+print(lats[14])
+plt.show()
